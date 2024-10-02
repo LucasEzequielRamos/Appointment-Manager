8113fe41a8ccb59a37a9e1e8a33d989a552ec81f
Guada <guadapdls@gmail.com>
2024-10-01 23:45:40 -0300

    register form try, has some bugs

--- src/app/auth/register/admin/page.tsx
+++ src/app/auth/register/admin/page.tsx
@@ -1,6 +1,133 @@
-const page = () => {
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/Components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/Components/ui/form"
+import { Input } from "@/Components/ui/input"
+
+const formSchema = z
+  .object({
+    first_name: z.string().min(2, {
+      message: "El nombre debe contener al menos 2 caracteres.",
+    }),
+    last_name: z.string().min(2, {
+      message: "El apellido debe contener al menos 2 caracteres.",
+    }),
+    email: z.string().email({ message: "Email invalido" }),
+    password: z.string().min(8, {
+      message: "La contraseña debe contener al menos 8 caracteres",
+    }),
+    confirmPassword: z.string().min(8, {
+      message: "La confirmación de la contraseña debe tener al menos 8 caracteres",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Las contraseñas no coinciden",
+  });
+
+export function page() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema)
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const res = await fetch("/api/register/admin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    if (data.status === 201) router.push("/auth/login");
+
+  }
+
   return (
-    <div>page</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}
+      className="flex flex-col items-center justify-center">
+        <FormField
+          control={form.control}
+          name="first_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nombre</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="last_name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Apellido</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirmar contraseña</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Registrarse</Button>
+      </form>
+    </Form>
   )
 }
 
